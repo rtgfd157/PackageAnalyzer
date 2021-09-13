cd772bcbc5cc1fa62534e93334f7f9b46bcaf1ac
rtgfd157 <idanz1234u@gmail.com>
2021-09-13 07:51:50 +0300

    dropdownlist of algo to chose when searching

---
+++ App/packagesAnalyzerProj/frontend/src/components/NpmPredictionComponents/SearchForPredictionComponent/AlgorythmDropDownList.js
@@ -0,0 +1,89 @@
+import React from 'react';
+import { Dropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap';
+
+class AlgorythmDropDownList extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+            list_algo:[],
+          is_mount :false,
+          dropdownOpen: false 
+          
+      };
+      this.fetch_data = this.fetch_data.bind(this);
+      
+      this.get_all_algo = 'http://127.0.0.1:8000/api/mlalgorithms/' ; 
+      this.Styling = { padding:"0.5rem", margin: '1%'};
+
+
+      this.handle_change_algorythm_list = this.props.handle_change_algorythm_list.bind(this);
+      this.toggle = this.toggle.bind(this);
+      this.setLastClicked = this.setLastClicked.bind(this);
+
+      
+
+
+      
+      
+    }
+
+    setLastClicked(val){
+        console.log(val);
+
+    }
+
+     toggle(){
+        
+          this.setState(prevState => ({   dropdownOpen: !prevState.dropdownOpen }));
+          console.log(this.state.dropdownOpen )
+
+    }
+
+    async componentDidMount() {
+        let d= await this.fetch_data(this.get_all_algo);
+        this.setState({ list_algo : d});
+       
+      }
+
+      async fetch_data(adress){
+        return  await  fetch(adress)
+       .then(response => response.json())
+       .catch(error => {
+        console.log('error in fetch');
+        throw(error);
+        
+        })}
+  
+    render() {
+      
+      return (
+                  <div>
+                    <p>  algo of Prediction:  <u></u> </p>
+                   
+                   
+
+
+                        <Dropdown isOpen={this.state.dropdownOpen} toggle={this.toggle}>
+                        <DropdownToggle caret>
+                        Dropdown
+                        </DropdownToggle>
+                        <DropdownMenu>
+                        
+
+                        {this.state.list_algo.map(algo => <div key={algo.id}>
+                            <DropdownItem>{algo.name}</DropdownItem> </div>
+                                   ) } 
+                            
+                        <DropdownItem onClick={() => this.setLastClicked(1)}> jh</DropdownItem>
+                        </DropdownMenu>
+                        </Dropdown>
+            </div>
+            ) 
+
+           
+                
+                
+    }
+  }
+  
+  export default AlgorythmDropDownList  
